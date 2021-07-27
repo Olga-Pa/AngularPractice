e79eaa0d1e035b6eb69927dccc6b2dfdc9cdf86a
Olga Pakhotina <olgapakhotina81@gmail.com>
2021-07-27 12:26:45 -0400

    new project

--- src/app/components/car/car.component.ts
+++ src/app/components/car/car.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
+ name:any;
+  speed:any;
+ model: string = 'RS8';
+ colors:any;
+//options:string[];
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+     this.name = "Audi";
+     this.speed = 110;
+    // this.model = 'RS8';
+    this.colors {
+      car: 'White',
+      salon: 'Black',
+      wheels: 'Silver';
+    };
+//this.options = ["gas", "drive", "parking"]
   }
-
 }
+// interface Colors {
+//   var car: string,
+//   var salon: string,
+//   var wheels: string
+// }
